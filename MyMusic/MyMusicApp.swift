7adde7630c008af9aaf89a207d13c79a29bc68cb
tetsu1026 <tetesu.w1026@gmail.com>
2021-02-24 13:06:21 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by Tetsuro Watanabe on 2021/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
